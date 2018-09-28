248c23aa6b6894399083ce4a49913cf47ab7a311
Adam Bronte <adam.bronte@coupa.com>
2018-09-28 15:44:15 -0700

    basic support for selecting dataframe columns by attribute #2

--- pyspark_proxy/sql/dataframe.py
+++ pyspark_proxy/sql/dataframe.py
@@ -6,7 +6,22 @@ from pyspark_proxy.sql.column import Column
 __all__ = ['DataFrame']
 
 class DataFrame(Proxy):
+    _functions = [
+            'toJSON', 'registerTempTable', 'createTempView', 'createOrReplaceTempView',
+            'createGlobalTempView', 'createOrReplaceGlobalTempView', 'printSchema',
+            'explain', 'exceptAll', 'isLocal', 'show', 'checkpoint', 'localCheckpoint',
+            'withWatermark', 'hint', 'count', 'collect', 'toLocalIterator', 'limit',
+            'take', 'foreach', 'foreachPartition', 'cache', 'persist', 'storageLevel',
+            'unpersist', 'coalesce', 'repartition', 'distinct', 'sample', 'samplyBy',
+            'randomSplit', 'colRegex', 'alias', 'crossJoin', 'join', 'sortWithinPartitions',
+            'sort', 'describe', 'summary', 'head', 'first', 'select', 'selectExpr',
+            'filter', 'groupBy', 'rollup', 'cube', 'agg', 'union', 'unionAll',
+            'unionByName', 'intersect', 'subtract', 'dropDuplicates', 'dropna', 'fillna',
+            'replace', 'approxQuantile', 'corr', 'cov', 'crosstab', 'feqItems',
+            'withColumn', 'withColumnRenamed', 'drop', 'toDF', 'toPandas']
+
     _column_obj = {}
+    _columns = []
 
     def __init__(self, id):
         self._id = id
@@ -17,7 +32,10 @@ class DataFrame(Proxy):
 
     @property
     def columns(self):
-        return self._call(self._id, 'columns', ((), {}))
+        if self._columns == []:
+            self._columns = self._call(self._id, 'columns', ((), {}))
+
+        return self._columns
 
     @property
     def write(self):
@@ -26,10 +44,24 @@ class DataFrame(Proxy):
     # retrieves column by key lookup
     # df['age']
     def __getitem__(self, item):
-        if item not in self._column_obj:
-            res = self._get_item(item)
-            col = Column(res['id'], self._id, item)
+        if item not in self.columns:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, item))
         else:
-            col = self._column_obj[item]
+            if item not in self._column_obj:
+                res = self._get_item(item)
+                col = Column(res['id'], self._id, item)
+            else:
+                col = self._column_obj[item]
 
-        return col
+            return col
+
+    # retrieves column by attribute
+    # df.age
+    def __getattr__(self, item):
+        if item in self._functions:
+            def method(*args, **kwargs):
+                return self._call(self._id, item, (args, kwargs))
+
+            return method
+        else:
+            return self[item]
--- tests/test_column.py
+++ tests/test_column.py
@@ -13,7 +13,8 @@ class ColumnTestCase(BaseTestCase):
         self.current_path = os.path.abspath(os.path.dirname(__file__))
         self.output_path = os.path.join(self.current_path, 'test_output')
 
-        self.df = self.sqlContext.read.json(os.path.join(self.current_path, 'data', 'data.json'))
+        data = [(1,2,'a'),(3,4,'b'),(5,6,'c')]
+        self.df = self.sqlContext.createDataFrame(data, ['foo', 'bar', 'baz'])
 
     def tearDown(self):
         if os.path.exists(self.output_path):
@@ -24,11 +25,15 @@ class ColumnTestCase(BaseTestCase):
 
         self.assertEqual(type(c), Column)
 
-    def test_select_with_column(self):
-        c = self.df['bar']
-        res_df = self.df.select(c)
+    def test_column_get_attr(self):
+        c = self.df.bar
 
-        self.assertEqual(res_df.columns, ['bar'])
+        self.assertEqual(type(c), Column)
+
+    def test_select_with_column(self):
+        res_df = self.df.select(self.df['bar'], self.df.foo)
+
+        self.assertEqual(res_df.columns, ['bar', 'foo'])
 
     def test_column_alias(self):
         res_df = self.df.select(self.df['foo'].alias('new_name'))
