00d7dd58a6870d86d8321bde1005c7a174220148
Adam Bronte <adam.bronte@coupa.com>
2018-10-08 13:28:38 -0700

    bump version and fix manifest for new readme

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst') as fp:
 
 setup(
     name='PysparkProxy',
-    version='0.0.6',
+    version='0.0.8',
     packages=[
         'pyspark_proxy',
         'pyspark_proxy.server',
