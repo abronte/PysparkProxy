ed80c164849387c0efac33521f83a114b691469f
Adam Bronte <adam.bronte@coupa.com>
2018-11-06 12:12:59 -0800

    bump pypi version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst') as fp:
 
 setup(
     name='PysparkProxy',
-    version='0.0.11',
+    version='0.0.12',
     packages=[
         'pyspark_proxy',
         'pyspark_proxy.server',
