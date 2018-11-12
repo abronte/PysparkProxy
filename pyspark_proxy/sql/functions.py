1296a22b9f6d42d21b9380d94801b4ccfaebcd3f
Adam Bronte <adam.bronte@coupa.com>
2018-11-12 13:55:05 -0800

    implements the rest of pyspark.sql.functions #21

--- pyspark_proxy/sql/functions.py
+++ pyspark_proxy/sql/functions.py
@@ -19,14 +19,15 @@ class Function(Proxy):
 
 
 def _create_function(name):
-    def _(col):
-        return Function(name, col).obj
+    def _(*args, **kwargs):
+        return Function(name, *args, **kwargs).obj
 
     return _
 
-# regular functions that return a column
-# from pyspark, 1.3, 1.4, 1.6, 2.1 functions
 _functions = [
+    # regular functions that return a column
+    # these are defined in an array in pyspark
+    # from pyspark, 1.3, 1.4, 1.6, 2.1 and string functions
     'lit', 'col', 'column', 'asc', 'desc', 'upper', 'lower'
     'sqrt', 'abs', 'max', 'min', 'count', 'sum', 'avg', 'mean',
     'sumDistinct', 'acos', 'asin', 'atan', 'cbrt', 'ceil', 'cos',
@@ -34,17 +35,39 @@ _functions = [
     'rint', 'signum', 'sin', 'sinh', 'tan', 'tanh', 'bitwiseNOT',
     'stddev', 'stddev_samp', 'stddev_pop', 'variance', 'var_samp',
     'var_pop', 'skewness', 'kurtosis', 'collect_list',
-    'collect_set', 'degrees', 'radians'
+    'collect_set', 'degrees', 'radians',
+    'ascii', 'base64', 'unbase64', 'initcap', 'lower', 'upper',
+    'reverse', 'ltrim', 'rtrim', 'trim',
+    # functions with individually implemented in pyspark
+    'approx_count_distinct', 'broadcast', 'coalsece', 'corr',
+    'covar_pop', 'covar_samp', 'countDistinct', 'first',
+    'grouping', 'grouping_id', 'input_file_name', 'isnan',
+    'isnull', 'last', 'monotonically_increasing_id', 'nanvl',
+    'rand', 'randn', 'round', 'bround', 'shiftLeft',
+    'shiftRight', 'shiftRightUnsigned', 'spark_parition_id',
+    'expr', 'struct', 'greatest', 'least', 'when', 'log',
+    'log2', 'conv', 'factorial', 'lag', 'lead', 'ntile',
+    'current_date', 'current_timestamp', 'date_format',
+    'year', 'quarter', 'month', 'dayofweek', 'dayofmonth',
+    'dayofyear', 'hour', 'minute', 'second', 'weekofyear',
+    'date_add', 'date_sub', 'datediff', 'add_months',
+    'months_between', 'to_date', 'to_timestamp', 'trunc',
+    'date_trunc', 'next_day', 'last_day', 'from_unixtime',
+    'unix_timestamp', 'from_utc_timestamp', 'to_utc_timestamp',
+    'window', 'crc32', 'md5', 'sha1', 'shsa2', 'hash',
+    'concat', 'concat_ws', 'decode', 'encode', 'format_number',
+    'format_string', 'instr', 'substring', 'substring_index',
+    'levenshtein', 'locate', 'lpad', 'rpad', 'repeat',
+    'split', 'regexp_extract', 'regexp_replace', 'initcap',
+    'soundex', 'bin', 'hex', 'unhex', 'length', 'translate',
+    'create_map', 'array', 'array_contains', 'explode',
+    'posexplode', 'explode_outer', 'posexplode_outer',
+    'get_json_object', 'json_tuple', 'from_json', 'to_json',
+    'size', 'sort_array', 'map_keys', 'map_values'
     ]
 
 for _name in _functions:
     globals()[_name] = _create_function(_name)
-
-def approx_count_distinct(col, rsd=None):
-    return Function('approx_count_distinct', col, rsd).obj
-
-def broadcast(df):
-    return Function('broadcast', df).obj
 
 # in the pyspark code, returnType defaults to the
 # StringType() datatype but for some reason it errors
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -36,5 +36,22 @@ class FunctionTestCase(BaseTestCase):
 
         self.assertEqual(type(df), DataFrame)
 
+    def test_function_count_distinct(self):
+        res = self.df.agg(F.countDistinct('foo', 'bar').alias('cnt')).collect()
+
+        self.assertEqual(res[0]['cnt'], 3)
+
+    def test_function_input_file_name(self):
+        res = self.df.select(F.input_file_name().alias('f')).collect()
+
+        self.assertEqual(res[0]['f'], '')
+
+    def test_function_concat(self):
+        res = self.df.select(F.concat(self.df.bar, self.df.baz).alias('concated')).collect()
+
+        self.assertEqual(res[0]['concated'], '2a')
+        self.assertEqual(res[1]['concated'], '4b')
+        self.assertEqual(res[2]['concated'], '6c')
+
 if __name__ == '__main__':
     unittest.main()
