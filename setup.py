b0e7af7d0125b78b001ef53ada7e3781c8e095fc
Adam Bronte <adam.bronte@coupa.com>
2018-11-13 09:47:40 -0800

    pypi 0.0.14 version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst') as fp:
 
 setup(
     name='PysparkProxy',
-    version='0.0.13',
+    version='0.0.14',
     packages=[
         'pyspark_proxy',
         'pyspark_proxy.server',
